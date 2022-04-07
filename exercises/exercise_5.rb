d82db088995253ecaa22f7c97b583818d9004cc0
Martha Staus <mjstaus@gmail.com>
2022-04-07 12:13:05 -0700

    FEATURE: Complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+
+num_stores = Store.count
+
+average_revenue = total_revenue / num_stores
+puts average_revenue
+
+num_stores_over_one_million = Store.where("annual_revenue > :annual_revenue", {annual_revenue: 1000000}).count
+puts num_stores_over_one_million
