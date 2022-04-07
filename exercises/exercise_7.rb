bc6ca3887ba0932739616d80ee586aaed97646a3
Martha Staus <mjstaus@gmail.com>
2022-04-07 12:53:26 -0700

    FEATURE: Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter new store name"
+print "> "
+
+new_store_name = $stdin.gets.chomp
+
+new_store = Store.create(name: new_store_name)
+
+puts new_store.errors.full_messages
