82173dcb73ec6a636d6380637c6a6bfbd763b331
Martha Staus <mjstaus@gmail.com>
2022-04-07 12:22:01 -0700

    FEATURE: Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,23 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Magic", last_name: "Mike", hourly_rate: 35)
+@store1.employees.create(first_name: "Lucia", last_name: "McBubbins", hourly_rate: 35)
+@store1.employees.create(first_name: "Figaro", last_name: "Farfel", hourly_rate: 35)
+
+@store2.employees.create(first_name: "Ellie", last_name: "Meatball", hourly_rate: 45)
+@store2.employees.create(first_name: "Elephant", last_name: "Ears", hourly_rate: 15)
+@store2.employees.create(first_name: "Elmer", last_name: "Erasmus", hourly_rate: 15)
+
+
+
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
